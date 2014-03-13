357e7f7289764badaed2c7740ce720adf2d619f2
lucyq <lucyzqin@gmail.com>
2014-03-12 23:23:21 -0400

    meow

--- transit.js
+++ transit.js
@@ -5,7 +5,7 @@
 // create a map
 var map;
 
-function init(){
+function initialize(){
 	var mapOptions = {
 		zoom: 13;
 		center: new google.maps.LatLng(-34.397, 150.644)
@@ -13,11 +13,6 @@ function init(){
 
 	map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
-	getGeoLocation();
-
-}
-
-function getGeoLocation() {
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function(position) {
 			var pos = new google.maps.LatLng(position.coords.latitude,
@@ -36,5 +31,5 @@ function getGeoLocation() {
 }
 
 
-google.maps.event.addDomListener(window, 'load', init);
+google.maps.event.addDomListener(window, 'load', initialize);
 
