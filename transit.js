bb19c01aaeafe2e7af473c7a19746234be7d3ec8
lucyq <lucyzqin@gmail.com>
2014-03-13 18:41:33 -0400

    fixing markers

--- transit.js
+++ transit.js
@@ -1,3 +1,39 @@
+//
+// RED LINE LOCATIONS
+//
+
+var Alewife	= new google.maps.LatLng(42.395428,-71.142483);
+var Andrew	= new google.maps.LatLng(42.330154, -71.057655);
+var Ashmont	= new google.maps.LatLng(42.284652, -71.064489);
+var Braintree = new google.maps.LatLng(42.2078543,-71.0011385);
+var Broadway = new google.maps.LatLng(42.342622,-71.056967);
+var Central = new google.maps.LatLng(42.365486,-71.103802);
+var Charles	= new google.maps.LatLng(42.361166,-71.070628); //charles mgh
+var Davis = new google.maps.LatLng(42.39674,-71.121815); 
+var DCross = new google.maps.LatLng(42.355518,-71.060225); // downtown crossing
+var Fields = new google.maps.LatLng(42.300093,-71.061667);
+var Harvard = new google.maps.LatLng(42.373362,-71.118956);
+var JFK = new google.maps.LatLng(42.320685,-71.052391); // jfk-umass
+var Kendall	= new google.maps.LatLng(42.36249079,-71.08617653);
+var NQuincy = new google.maps.LatLng(42.275275,-71.029583);
+var Park = new google.maps.LatLng(42.35639457,-71.0624242);
+var Porter = new google.maps.LatLng(42.3884,-71.119149);
+var QuincyA = new google.maps.LatLng(42.233391,-71.007153);
+var QuincyC = new google.maps.LatLng(42.251809,-71.005409);
+var Savin = new google.maps.LatLng(42.31129,-71.053331); // Savin Hill
+var Shawmut	= new google.maps.LatLng(42.29312583,-71.06573796);
+var South = new google.maps.LatLng(42.352271,-71.055242);
+var Wollaston = new google.maps.LatLng(42.2665139,-71.0203369);
+
+var redLine = [Alewife, Andrew, Ashmont, Braintree, Broadway, Central, Charles,
+				Davis, DCross, Fields, Harvard, JFK, Kendall, NQuincy, Park, Porter, 
+				QuincyA, QuincyC, Savin, Shawmut, South, Wollaston];
+var redLineMarkers = new Array();
+
+
+
+
+
 
 // if you put a var in front of a variable, it means that your variable is only good within this scope
 
@@ -111,39 +147,6 @@ function genMarkers() {
 		});
 	}
 }
-
-
-//
-// RED LINE MARKERS
-//
-
-var Alewife	= new google.maps.LatLng(42.395428,-71.142483);
-var Andrew	= new google.maps.LatLng(42.330154, -71.057655);
-var Ashmont	= new google.maps.LatLng(42.284652, -71.064489);
-var Braintree = new google.maps.LatLng(42.2078543,-71.0011385);
-var Broadway = new google.maps.LatLng(42.342622,-71.056967);
-var Central = new google.maps.LatLng(42.365486,-71.103802);
-var Charles	= new google.maps.LatLng(42.361166,-71.070628); //charles mgh
-var Davis = new google.maps.LatLng(42.39674,-71.121815); 
-var DCross = new google.maps.LatLng(42.355518,-71.060225); // downtown crossing
-var Fields = new google.maps.LatLng(42.300093,-71.061667);
-var Harvard = new google.maps.LatLng(42.373362,-71.118956);
-var JFK = new google.maps.LatLng(42.320685,-71.052391); // jfk-umass
-var Kendall	= new google.maps.LatLng(42.36249079,-71.08617653);
-var NQuincy = new google.maps.LatLng(42.275275,-71.029583);
-var Park = new google.maps.LatLng(42.35639457,-71.0624242);
-var Porter = new google.maps.LatLng(42.3884,-71.119149);
-var QuincyA = new google.maps.LatLng(42.233391,-71.007153);
-var QuincyC = new google.maps.LatLng(42.251809,-71.005409);
-var Savin = new google.maps.LatLng(42.31129,-71.053331); // Savin Hill
-var Shawmut	= new google.maps.LatLng(42.29312583,-71.06573796);
-var South = new google.maps.LatLng(42.352271,-71.055242);
-var Wollaston = new google.maps.LatLng(42.2665139,-71.0203369);
-
-var redLine = [Alewife, Andrew, Ashmont, Braintree, Broadway, Central, Charles,
-				Davis, DCross, Fields, Harvard, JFK, Kendall, NQuincy, Park, Porter, 
-				QuincyA, QuincyC, Savin, Shawmut, South, Wollaston];
-var redLineMarkers = new Array();
 
 
 
