ebdfd29660c41e97a25c9e8d76130d464a1ca0b3
lucyq <lucyzqin@gmail.com>
2014-03-13 14:10:42 -0400

    added marker

--- transit.js
+++ transit.js
@@ -1,14 +1,37 @@
+var me = new google.maps.LatLng(42.3581, 71.0636)
+
 function initialize() {
 	var mapOptions = {
 		zoom: 13,
-		center: new google.maps.LatLng(-34.497, 160.755)
+		center: me
 	};
 
 	var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
 }
 
+var marker = new google.maps.Marker({
+	position: me,
+	map: map,
+	title: "HI, YOU FOUND ME!"
+});
+
+
 google.maps.event.addDomListener(window, 'load', initialize);
 
+
+
+/*
+function getLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			myLat = position.coords.latitude;
+			myLng = position.coords.longitude;
+			renderMap();
+		})
+	}
+
+}
+*/
 
 /* NOTES
 google.maps.Map - The map object (duh!)
