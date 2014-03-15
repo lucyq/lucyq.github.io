3194c8264b30e768bd085071399880cc20baf944
lucyq <lucyzqin@gmail.com>
2014-03-14 23:27:39 -0400

    fixing syntax

--- transit.js
+++ transit.js
@@ -202,7 +202,7 @@ function genMarkers(length, colorLine, icon, color){
 			position: station,
 			icon: icon,
 			map: map,
-			title: color[i]["Name"];
+			title: color[i]["Name"]
 		});
 
 		// create windows
