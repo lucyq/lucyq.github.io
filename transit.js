d89bc541442403e65cae69527f9d30e47886e3c6
lucyq <lucyzqin@gmail.com>
2014-03-14 21:42:21 -0400

    debugging

--- transit.js
+++ transit.js
@@ -255,6 +255,7 @@ function findDistance (lat1, lat2, lon1, lon2) {
     	    Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
 	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
 	var d = R * c;
+	console.log(d);
 
 	return d;
 
