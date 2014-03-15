7cb4ce135bbec98da7c6af7426a1016397ee29f6
lucyq <lucyzqin@gmail.com>
2014-03-15 00:37:22 -0400

    sdfs

--- transit.js
+++ transit.js
@@ -271,6 +271,8 @@ var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
         Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
 var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
 var d = R * c;
+
+return d;
 }
 
 
