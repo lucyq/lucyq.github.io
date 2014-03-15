9b3bb72828e44d1b95941c24f0fb4072134e5211
lucyq <lucyzqin@gmail.com>
2014-03-14 20:40:51 -0400

    asfsd

--- transit.js
+++ transit.js
@@ -187,12 +187,14 @@ function genMarkers(length, colorLine, icon, color){
 		station = new google.maps.LatLng(colorLine[i]["Lat"], colorLine[i]["Lng"]);
 		stationArray[i] = station;
 		
+		// create markers
 		var stationMark = new google.maps.Marker({
 			position: station,
 			icon: icon,
 			map: map
 		});
 	}
+	// create polyline
 	var flightPath = new google.maps.Polyline({
 		path: stationArray,
 		geodesic: true,
@@ -202,6 +204,24 @@ function genMarkers(length, colorLine, icon, color){
 		map: map
 	});
 
+}
+
+function findDistance () {
+	Number.prototype.toRad = function() {
+		return this * Math.PI/180;
+	}
+	var R = 6371; // km
+	var dLat = (lat2-lat1).toRad();
+	var dLon = (lon2-lon1).toRad();
+	var lat1 = lat1.toRad();
+	var lat2 = lat2.toRad();
+
+	var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    	    Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	var d = R * c;
+
+	alert(d);
 }
 
 
@@ -245,13 +265,6 @@ content
 
 
 
-
-
-
-//
-// CODE FROM CLASS
-// 
-data = JSON.parse (responseText);
 
 // to print out a line:
 // console.log(data.line);
