203efc817aa39eef856cb578beea9f923a3da63e
lucyq <lucyzqin@gmail.com>
2014-03-15 00:36:26 -0400

    fixing errors

--- transit.js
+++ transit.js
@@ -261,18 +261,16 @@ function findDistance (lat1, lat2, lon1, lon2) {
 	Number.prototype.toRad = function() {
 		return this * Math.PI/180;
 	}
-	var R = 6371; // km
-	var dLat = (lat2-lat1).toRad();
-	var dLon = (lon2-lon1).toRad();
-	var lat1 = lat1.toRad();
-	var lat2 = lat2.toRad();
+var R = 6371; // km
+var dLat = (lat2-lat1).toRad();
+var dLon = (lon2-lon1).toRad();
+var lat1 = lat1.toRad();
+var lat2 = lat2.toRad();
 
-	var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-    	    Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
-	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-	var d = R * c;
-
-	return d;
+var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+var d = R * c;
 }
 
 
