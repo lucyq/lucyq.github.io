9f3545a916ee79912f26c16a095718a0811f6d44
lucyq <lucyzqin@gmail.com>
2014-03-15 23:16:49 -0400

    sfs

--- transit.js
+++ transit.js
@@ -241,10 +241,9 @@ console.log(endPoint);
 				listItem = document.createElement("li");
 				listItem.innerHTML = "Arriving in: " + foundSeconds[j] + " seconds";
 				list.appendChild(listItem);
+				infoDiv.appendChild(list); 
+				console.log(foundSeconds);
 			}
-		infoDiv.appendChild(list);
-
-		
 		}
 		
 
