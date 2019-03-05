63f1314ea0df4aa04413fb9f3a1f9b9f91653136
lucyq <lucyzqin@gmail.com>
2019-03-04 22:31:15 -0500

    changed content

---
+++ scripts.js
@@ -0,0 +1,37 @@
+	// 	/* TOGGLING DIFF SECTIONS */
+	// 	$(".nav_link").bind("click", function() {
+  //     $(".info").hide();
+  //     var divId = $(this).attr("divId");
+  //  //	$("#" + divId).slideToggle('slow', function() {});
+  //     $("#" + divId).show();
+  //  });
+  //  /* STICKY NAV */
+  //  $(document).ready(function() {  
+  //    var stickyNavTop = $('.nav').offset().top;  
+       
+  //    var stickyNav = function(){  
+  //      var scrollTop = $(window).scrollTop();  
+              
+  //      if (scrollTop > stickyNavTop) {   
+  //          $('.nav').addClass('sticky');  
+  //      } else {  
+  //          $('.nav').removeClass('sticky');   
+  //      }  
+  //    };  
+       
+  //    stickyNav();  
+       
+  //    $(window).scroll(function() {  
+  //        stickyNav();  
+  //    });  
+  //  });  
+  //  /* SLIDER */
+  //  $('.variable-width').slick({
+  //    dots: true,
+  //    infinite: true,
+  //    speed: 300,
+  //    slidesToShow: 3,
+  //    centerMode: true,
+  //    variableWidth: true
+  //  });
+           
